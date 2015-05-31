56de543538c72b5d30c18d4211d0fd4018cb08b2
Casey Bisson <casey.bisson@joyent.com>
2015-05-31 15:28:04 -0700

    test two different u/p combos

--- bin/configure-couchbase.bash
+++ bin/configure-couchbase.bash
@@ -32,7 +32,15 @@ COUCHBASERESPONSIVE=0
 while [ $COUCHBASERESPONSIVE != 1 ]; do
     echo -n '.'
     sleep .7
+
+    # test the default u/p
     couchbase-cli server-list -c 127.0.0.1:8091 -u access -p password &> /dev/null
+    if [ $? -eq 0 ]; then
+        let COUCHBASERESPONSIVE=1
+    fi
+
+    # test the alternate u/p
+    couchbase-cli server-list -c 127.0.0.1:8091 -u Administrator -p password &> /dev/null
     if [ $? -eq 0 ]; then
         let COUCHBASERESPONSIVE=1
     fi
