4a59d6a31c7bf670a1236cc4f4bf4d26379b7ea1
Casey Bisson <casey.bisson@joyent.com>
2015-06-02 09:47:11 -0700

    set CPU limits on Triton

--- bin/install-triton-centos.bash
+++ bin/install-triton-centos.bash
@@ -21,13 +21,40 @@ if [ ! -d "/opt/couchbase/" ]; then
 fi
 
 echo '#'
+echo '# Setting CPU limits for Couchbase and restarting'
+echo '#'
+
+# insert Triton-specific config details into the startup script
+cat /opt/couchbase/bin/couchbase-server | \
+sed "/export PATH/a export ERL_AFLAGS" | \
+sed "/export PATH/a ERL_AFLAGS='+S $MYCPUS'" | \
+sed "/export PATH/a export COUCHBASE_NS_SERVER_VM_EXTRA_ARGS" | \
+sed "/export PATH/a COUCHBASE_NS_SERVER_VM_EXTRA_ARGS=$(printf '["+S", "%s"]' $MYCPUS)" | \
+sed "/export PATH/a \# CPU limits to keep Couchbase from trying to schedule across all the CPUs it can see" | \
+sed '/export PATH/a
+' \
+> /root/couchbase-server-config.tmp
+
+# copy the temp back to the primary and delete the temp
+cat /root/couchbase-server-config.tmp > /opt/couchbase/bin/couchbase-server
+rm -f /root/couchbase-server-config.tmp
+
+# restart Couchbase
+/etc/init.d/couchbase-server restart
+
+echo '#'
 echo '# Waiting for Couchbase to start'
 echo '#'
+
 while [ ! -f "/opt/couchbase/var/lib/couchbase/couchbase-server.pid" ]; do
     echo -n '.'
     sleep 1.3
 done
 sleep 2
+
+# set the number of writers
+/opt/couchbase/bin/cbepctl 127.0.0.1:11210 -b default set flush_param max_num_writers $(($MYCPUS>1?$MYCPUS/2:1))
+
 
 # configure Couchbase
 curl -sL https://raw.githubusercontent.com/misterbisson/couchbase-benchmark/master/bin/configure-couchbase.bash | bash -s $MYMEMORY benchmark $MYIPPRIVATE $MYIPPUBLIC
